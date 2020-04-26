4149cc3359c3d86b63e0c72c13a2b2abb5ef7aad
edblanco <edblanco0712@gmail.com>
2020-04-26 15:07:20 +0200

    Adding AOP

---
+++ src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,51 @@
+package com.luv2code.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @Before: calling method: " + theMethod);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for (Object tempArg : args) {
+			myLogger.info("=====>> argument: " + tempArg);
+		}
+	}
+	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @afterReturning: calling method: " + theMethod);
+		
+		myLogger.info("=====>> result: " + theResult);
+	}
+}
